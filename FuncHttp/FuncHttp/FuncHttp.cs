b736dd727e64b90f05e29200b608c672f51ee840
hinault <ngdonfack@gmail.com>
2019-11-08 14:07:54 +0100

    Mise à jour fonction

--- FuncHttp/FuncHttp/FuncHttp.cs
+++ FuncHttp/FuncHttp/FuncHttp.cs
@@ -21,6 +21,7 @@ namespace FuncHttp
 
             string name = req.Query["name"];
 
+            
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
